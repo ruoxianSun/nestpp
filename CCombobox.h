defdd5995080f2f716038d48fd60703fb75b2415
zhenghm <haimei.zheng@LuxCreo.com>
2020-07-01 17:21:36 +0800

    add:qproperty

--- CCombobox.h
+++ CCombobox.h
@@ -4,6 +4,10 @@
 #include <QComboBox>
 #include <QStyledItemDelegate>
 class CComboBoxDelegate:public QStyledItemDelegate{
+    Q_OBJECT
+    Q_PROPERTY(QColor textColor READ getTextColor WRITE setTextColor)
+    Q_PROPERTY(QColor textColorHover READ getTextColorHover WRITE setTextColorHover)
+    Q_PROPERTY(QColor textColorPressed READ getTextColorPressed WRITE setTextColorPressed)
 public:
     CComboBoxDelegate(QObject*parent=0):QStyledItemDelegate (parent){}
 
@@ -11,6 +15,15 @@ public:
     virtual void paint(QPainter *painter,
                        const QStyleOptionViewItem &option,
                        const QModelIndex &index) const;
+private:
+    void setTextColor(const QColor&color){textColor=color;}
+    QColor getTextColor()const{return textColor;}
+    void setTextColorHover(const QColor&color){textColorHover=color;}
+    QColor getTextColorHover()const{return textColorHover;}
+    void setTextColorPressed(const QColor&color){textColorPressed=color;}
+    QColor getTextColorPressed()const{return textColorPressed;}
+private:
+        QColor textColor,textColorHover,textColorPressed;
 
 };
 class CCombobox : public QComboBox
